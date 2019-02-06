718e1c6904920097568bd42b1c146bc6fcc3696d
Mew-www <mew.proxy@hotmail.com>
2019-02-06 17:01:57 +0200

    Alter initial settings and init a readme

--- gi_assignment/gi_assignment/settings.py
+++ gi_assignment/gi_assignment/settings.py
@@ -15,21 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Security
+SECRET_KEY = os.environ['GI_ASSIGNMENT_SECRET_KEY']
+ALLOWED_HOSTS = os.environ.get('GI_ASSIGNMENT_ALLOWED_HOSTS', "localhost").split(',')
+DEBUG = os.environ.get('GI_ASSIGNMENT_DEBUG') == "1"
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z3!4%)@8*@d8py7n_+x%^ok$ckgm))*02o^rt@rt!y5i%x)14y'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
+# Applications and functionality
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -69,10 +60,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gi_assignment.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+# Database(s) (for user auth and models)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -80,16 +68,17 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 7
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -99,22 +88,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
---
+++ readme.md
@@ -0,0 +1,41 @@
+### Required background software (installed and running)  
+* **NGINX + Gunicorn** *(described further below)* 
+    * or **Apache2 + mod_wsgi**
+        * *[Linux guide](https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/modwsgi/)* 
+        * *[Windows guide, except link Django's wsgi.py instead of described wsgi_app.py](https://beamtic.com/installing-mod-wsgi-apache-windows)*
+* Redis-server [[Linux/Debian](https://redislabs.com/ebook/appendix-a/a-1-installation-on-debian-or-ubuntu-linux/)] [[Mac/OSX](https://redislabs.com/ebook/appendix-a/a-2-installing-on-os-x/)] [[Win32/Win64](https://redislabs.com/ebook/appendix-a/a-3-installing-on-windows/)]  
+
+### Clone this repo and set-up virtualenv
+
+`git clone <repository url>`  
+
+`cd <repository name>`  
+
+`virtualenv venv --python=python3`  
+
+`source venv/bin/activate`  
+
+`pip install -r requirements.txt`  
+
+### Set-up environment variables and init database models
+
+Environment variables to declare (on the user running WSGI):  
+* GI_ASSIGNMENT_SECRET_KEY="a-instance-specific-new-secret-key"  
+
+    Use the following snippet to generate a new key:  
+    `from django.core.management.utils import get_random_secret_key`  
+    `get_random_secret_key()`
+      
+* GI_ASSIGNMENT_ALLOWED_HOSTS="comma,separated,list,of,domains"
+  
+    Optional, defaults to "localhost". If you're hosting/accessing locally from "127.0.0.1" you may have to change this to that. Or any other domain(s) being used.
+
+* GI_ASSIGNMENT_DEBUG="0"
+
+    Optional, defaults to False. Only value "1" sets it on.
+
+Migrate database models:  
+
+`cd gi_assignment`  
+
+`python manage.py migrate`
