48214b02c2ef12b731d8d14d57cf793525798370
Mew-www <mew.proxy@hotmail.com>
2019-02-07 20:50:36 +0200

    Implement tests for total and daily interfaces

--- gi_assignment/reporting/tests.py
+++ gi_assignment/reporting/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+import json
+import os
 
-# Create your tests here.
+
+class InterfaceTests(SimpleTestCase):
+    """
+    When using headers in interface tests, remember to pass in the already-HTTP_-prefixed (and underscored) version.
+    """
+    def test_total_v1_interface(self):
+        response = self.client.get(
+            '/reporting/v1/total/from-2017-05-01/to-2017-05-15',
+            follow=True,
+            **{'HTTP_X_GI_TOKEN': os.environ['GI_ASSIGNMENT_TEST_API_TOKEN']}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertCountEqual(
+            json.loads(response.content).keys(),
+            ['total_conversation_count', 'total_user_message_count', 'total_visitor_message_count']
+        )
+
+    def test_daily_v1_interface(self):
+        response = self.client.get(
+            '/reporting/v1/daily/from-2017-05-01/to-2017-05-15',
+            follow=True,
+            **{'HTTP_X_GI_TOKEN': os.environ['GI_ASSIGNMENT_TEST_API_TOKEN']}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertCountEqual(
+            json.loads(response.content).keys(),
+            ['by_date', 'paginated', 'current_page', 'max_page']
+        )
